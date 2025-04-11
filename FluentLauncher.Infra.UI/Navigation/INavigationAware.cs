87cf7266e4e8c02137e62ca399db07ce33be178a
natsurainko <a-275@qq.com>
2025-04-11 14:17:26 +0800

    从 Fluent Launcher 项目中分包管理

---
+++ FluentLauncher.Infra.UI/Navigation/INavigationAware.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentLauncher.Infra.UI.Navigation;
+
+/// <summary>
+/// Implemented by a view model that can respond to navigation events
+/// </summary>
+public interface INavigationAware
+{
+    void OnNavigatedTo(object? parameter) { }
+
+    void OnNavigatedFrom() { }
+}
